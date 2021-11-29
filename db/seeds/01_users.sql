5ef3e7c63b4b6bbddcf309c0b6968f244e82c868
WenConrad <conradwen@gmail.com>
2021-11-29 14:39:44 -0800

    add seeds to database

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,53 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, phone)
+VALUES (
+  'Jonathan Bell',
+  'jbellcanada@gmail.com'
+  'password',
+  '1-800-767-1123'
+), (
+  'Timothy DeLaney',
+  'timothydalton007@gmail.com',
+  'password',
+  '1-787-663-9999'
+), (
+  'Persephone Lee',
+  'peelee142@realestate.com',
+  'password',
+  '1-888-888-8888'
+), (
+  'Herbert Langley',
+  'HangleyLerbert@hlangley.com',
+  'password',
+  '1-234-222-3786'
+), (
+  'Reena Paul',
+  'rpauleena@affordablehomes.com',
+  'password',
+  '1-999-999-9999'
+), (
+  'Lanika Lost',
+  'lol@hahaha.com',
+  'password',
+  '1-707-707-9999'
+), (
+  'James Burkins',
+  'jburk@affordablehomes.com',
+  'password',
+  '1-234-599-9799'
+), (
+  'Bella Barker',
+  'itzbbellaa@realestate.com',
+  'password',
+  '1-999-767-8543'
+), (
+  'Bruce Wayne',
+  'imbatman@iwantanewbatcave.com',
+  'password',
+  '1-333-4-GOTHAM'
+), (
+  'Gawr Gura',
+  'apexpredator@hololive.com',
+  'password',
+  '1-899-765-4321'
+)
