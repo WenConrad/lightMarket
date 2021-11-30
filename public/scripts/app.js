6e840aa65fabef9a5786728c800414b49f9d15a7
Andres <andres039039@hotmail.com>
2021-11-30 15:33:26 -0500

    new template for cards following current ejs format, adds functionality to the favorite buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,55 +2,38 @@
 
 //$(document).ready(function () {
 
-
-  $(function( $ ){
-
-//renders content from the database into html
+$(function ($) {
+  //renders content from the database into html
   const displayProperties = function (arr) {
 
-    console.log(arr)
 
     return arr.properties.forEach((property) => {
+      $(".container")
+        .append(`<section class="listing-adds">  <div class="card" style="width: 18rem;">
 
-      $('.container').append(`<section class="listing-adds"> <div claconst displayListingInfo = function (arr) {
-      }"list-group-item">Number of bedrooms: ${property.bedrooms}</li><li class="list-group-item">Number of bathrooms: ${property.bathrooms}</li></ul></div></section>
-      <button type="submit" id='favorite' class="btn btn-outline-danger"><i class="far fa-heart"></i></button>`)
-
+      <div class="card-body"><li class="list-group-item">  <h5 class="card-title">${property.address}</h5>Number of bedrooms: ${property.bedrooms}</h5> <ul class="list-group list-group-flush"><li class="list-group-item">Number of bathrooms: ${property.bathrooms}</li></ul></div>
+      <div class="card-body card-body_links">
+      <button id='favorite-${property.id}' class="favorite btn btn-outline-danger"><i class="far fa-heart"></i></button></div></section>`);
+      $(`#favorite-${property.id}`).click(function () {
+        alert("This property has been added to your favorites");
+      });
     });
   };
 
-
-  //connects to the database, in this case is the db/seeds/widgets.sql
+  //connects to the database, in this case is the db/seeds/properties.sql
 
   const displayListings = () => {
-
     $.ajax({
-      url: '/api/properties',
-      method: 'GET'
+      url: "/api/properties",
+      method: "GET",
     })
-    .then(result => {
-      console.log(result)
-      displayProperties(result)
-    })
-    .catch(err =>
-      console.log(err.message))
-  }
+      .then((result) => {
+        console.log(result);
+        displayProperties(result);
+      })
+      .catch((err) => console.log(err.message));
+  };
 
-  displayListings()
-//renders all the necessary listings
-  const displayListingInfo = function (arr) {
-      console.log(arr)
-      return arr.properties.forEach((property) => {
+  displayListings();
 
-        $('.container').append(`<section class="listing-adds"> <div claconst displayListingInfo = function (arr) {
-
-        }"list-group-item">Number of bedrooms: ${property.bedrooms}</li><li class="list-group-item">Number of bathrooms: ${property.bathrooms}</li></ul></div></section>`)
-
-      });
-    };
-
-    $( 'button' ).click(function() {
-      alert( "This property has been added to your favorites" );
-    });
-  })
-
+});
