0eb97a5adda428ab4e9eab6c43c4387611674196
Andres <andres039039@hotmail.com>
2021-11-30 16:33:46 -0500

    merges two tables, listings and properties, and then displays price per property

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,7 +11,7 @@ $(function ($) {
       $(".container")
         .append(`<section class="listing-adds">  <div class="card" style="width: 18rem;">
 
-      <div class="card-body"><li class="list-group-item">  <h5 class="card-title">${property.address}</h5>Number of bedrooms: ${property.bedrooms}</h5> <ul class="list-group list-group-flush"><li class="list-group-item">Number of bathrooms: ${property.bathrooms}</li></ul></div>
+      <div class="card-body"><li class="list-group-item">  <h5 class="card-title">${property.address}</h5>Number of bedrooms: ${property.bedrooms}</h5> <ul class="list-group list-group-flush"><li class="list-group-item">Number of bathrooms: ${property.bathrooms}</li><li class="list-group-item">Listed for $${property.price}</li></ul></div>
       <div class="card-body card-body_links">
       <button id='favorite-${property.id}' class="favorite btn btn-outline-danger"><i class="far fa-heart"></i></button></div></section>`);
       $(`#favorite-${property.id}`).click(function () {
--- routes/properties.js
+++ routes/properties.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM properties;`)
+    db.query(`SELECT * FROM properties JOIN listings ON properties.id = listings.property_id;`)
       .then(data => {
         const properties = data.rows;
         res.json({ properties });
