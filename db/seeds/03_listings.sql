91b7879bd81a8b2680679038453239cf798e9c81
WenConrad <conradwen@gmail.com>
2021-11-30 14:35:48 -0800

    add property photos

--- db/seeds/03_listings.sql
+++ db/seeds/03_listings.sql
@@ -1,69 +1,81 @@
-INSERT INTO listings (property_id, listing_agent, price, listing_date)
+INSERT INTO listings (property_id, listing_agent, price, listing_date, photo_url)
 VALUES (
   1,
   3,
   543654,
-  '2021-07-22'
+  '2021-07-22',
+  'https://upload.wikimedia.org/wikipedia/commons/2/2f/Big_House%2C_Little_House_%282407030202%29.jpg'
 ), (
   2,
   3,
   254676,
-  '2021-09-04'
-), (
+  '2021-09-04',
+  'https://cdn.pixabay.com/photo/2020/01/23/11/02/sweden-4787445_960_720.jpg'
+  ), (
   3,
   4,
   123456,
-  '2021-11-03'
+  '2021-11-03',
+  'https://cdn.pixabay.com/photo/2019/12/25/01/56/apartment-building-4717689_1280.jpg'
 ), (
   4,
   5,
   373453,
-  '2021-11-17'
+  '2021-11-17',
+  'https://live.staticflickr.com/65535/48104840292_66525d41f1_b.jpg'
 ), (
   5,
   5,
   304234,
-  '2021-11-22'
+  '2021-11-22',
+  'https://cdn.pixabay.com/photo/2015/02/01/18/13/condo-619977_1280.jpg'
 ), (
   6,
   7,
   198364,
-  '2021-11-07'
+  '2021-11-07',
+  'https://libreshot.com/wp-content/uploads/2016/04/modern-apartment-house.jpg'
 ), (
   7,
   7,
   277885,
-  '2021-10-30'
+  '2021-10-30',
+  'https://live.staticflickr.com/4064/4447310246_6dc9847155_b.jpg'
 ), (
   8,
   8,
   477765,
-  '2021-11-15'
+  '2021-11-15',
+  'https://upload.wikimedia.org/wikipedia/commons/6/63/Dilapidated_apartment_yard_in_Regent_Park_%2844731511740%29.jpg'
 );
 
-INSERT INTO listings (property_id, listing_agent, price, listing_date, sold_date)
+INSERT INTO listings (property_id, listing_agent, price, listing_date, sold_date, photo_url)
 VALUES (
   9,
   8,
   896536,
   '2021-01-14',
-  '2021-01-22'
+  '2021-01-22',
+  'https://s0.geograph.org.uk/geophotos/06/28/71/6287139_7eff5f6e.jpg'
 ), (
   10,
   7,
   298745,
   '2021-02-17',
-  '2021-03-01'
+  '2021-03-01',
+  'https://cdn.stocksnap.io/img-thumbs/960w/apartment-building_2T9NFO4RNI.jpg'
 ), (
   11,
   7,
   242433,
   '2021-08-29',
-  '2021-09-13'
+  '2021-09-13',
+  'https://cdn.pixabay.com/photo/2020/04/15/01/24/apartment-building-5044805_1280.jpg'
 ), (
   12,
   5,
   354678,
   '2021-03-24',
-  '2021-04-02'
+  '2021-04-02',
+  'https://www.musees.qc.ca/var/storage/images/musees/guide/village-historique-de-val-jalbert/carrousel/village-historique-de-val-jalbert-05/338429-1-fre-CA/village-historique-de-val-jalbert-05.jpg'
 );
