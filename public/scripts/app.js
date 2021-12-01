f90e939e047379ee517aada23e2d7adcd9f0931b
Andres <andres039039@hotmail.com>
2021-12-01 13:56:54 -0500

    integrates new card form with jquery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,16 @@
 // Client facing scripts here
 
-  const getLimit = function () {
-    let limit;
-     $( '#filter').on('submit', function(event) {
-       event.preventDefault();
-       console.log('click')
-      $( '.listing-adds' ).empty();
-        limit = $( this)[0][0].valueAsNumber
-       //return this value as a number.
- })
- return limit;
-}
+//   const getLimit = function () {
+//     let limit;
+//      $( '#filter').on('submit', function(event) {
+//        event.preventDefault();
+//        console.log('click')
+//       $( '.listing-adds' ).empty();
+//         limit = $( this)[0][0].valueAsNumber
+//        //return this value as a number.
+//  })
+//  return limit;
+// }
 //return limit
 
 
@@ -19,29 +19,61 @@ $(function ($) {
   //renders content from the database into html
   const displayProperties = function (arr) {
 
-if (price < limit)
     return arr.properties.forEach((property) => {
-      $( '.listing-adds' ).append( `<div class="card" style="width: 18rem;">
-      <img src="./images/main/house1.png" class="card-img-top" alt="house1.jpg">
-      <div class="card-body">
-        <h5 class="card-title">${property.address}</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Price: $${property.price}</li>
-        <li class="list-group-item">#Bedrooms: ${property.bedrooms}</li>
-        <li class="list-group-item"># Bathrooms: ${property.bathrooms}</li>
-        <li class="list-group-item">Sqrt: ${property.sqft}</li>
-      </ul>
-      <div class="card-body card-body_links">
-        <form method="post" action="/favourite">
-          <button type="submit" id='favorite-${property.id}' class="btn btn-outline-danger"><i class="far fa-heart"></i></button>
-        </form>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-    </div>` )
+      $( '.listing-adds' ).append(`<div class="card">
 
+<img src="./images/main/house1.png" class="card-img-top" alt="house1.jpg">
+<div class="card-body">
+  <h5 class="card-title">${property.address}</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+</div>
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">Price: $${property.price}</li>
+  <li class="list-group-item">#Bedroom: ${property.bedrooms}</li>
+  <li class="list-group-item"># Bathroom: ${property.bathrooms}</li>
+  <li class="list-group-item">Sqrt: ${property.sqft}</li>
+</ul>
+<div class="card-body card-body_links">
+  <form method="post" action="/favourite">
+    <button type="submit" id='favorite-${property.id}' class="btn btn-outline-success"><i class="far fa-heart"></i></button>
+  </form>
+  <button type="button" id='test-button' class="btn btn-outline-secondary"><a href="mailto:username@lightmarket.com"><i class="far fa-envelope"></i></a></button>
+
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-outline-secondary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap"><i class="far fa-comments"></i></button>
+
+  <!-- pop up modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <!-- message send to database -->
+          <form method="get" action="/">
+            <div class="form-group">
+              <label for="message-text" class="col-form-label">Message:</label>
+              <textarea style="height: fit-content" class="form-control" id="message-text"></textarea>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Send message</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <form method="delete" action="">
+
+    <button type="button" id='test-button' class="btn btn-outline-danger"><i class="far fa-trash-alt"></i></a></button>
+  </form>
+</div>
+</div>`)
 
       $(`#favorite-${property.id}`).click(function () {
         alert("This property has been added to your favorites");
